f129472709a84b4e09d6d75b88977bbe8f007bbe
niksrinivas <niksrinivas23@gmail.com>
2020-10-29 20:02:30 -0500

    javaFX init

--- src/assignment5/CritterWorld.java
+++ src/assignment5/CritterWorld.java
@@ -3,20 +3,24 @@ package assignment5;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class CritterWorld extends Application {
+
+    int counter = 0;
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    int counter = 0;
 
     @Override
     public void start(Stage primaryStage) {
