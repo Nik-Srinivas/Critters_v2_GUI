553ed31aabb7d839fcfaba4670c53af15a162c84
Reza <rezamohideen@utexas.edu>
2020-11-02 06:08:37 -0600

    display grid

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -14,6 +14,18 @@
 
 package assignment5;
 
+import javafx.application.Application;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +33,7 @@ import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,205 +43,69 @@ import java.util.Scanner;
  * May not use 'test' argument without specifying input file.
  */
 
-public class Main {
+public class Main extends Application {
+    private static final int TILE_SIZE = 40;
+    private static final int W = Params.WORLD_WIDTH * 40;
+    private static final int H = Params.WORLD_HEIGHT * 40;
 
-    /* Scanner connected to keyboard input, or input file */
-    static Scanner kb;
+    private static final int X_TILES = W / TILE_SIZE;
+    private static final int Y_TILES = H / TILE_SIZE;
+    public static ByteArrayOutputStream testOutputString;
 
-    /* Input file, used instead of keyboard input if specified */
-    private static String inputFile;
+    private Square[][] grid = new Square[X_TILES][Y_TILES];
+    private Scene scene;
 
-    /* If test specified, holds all console output */
-    static ByteArrayOutputStream testOutputString;
+    private Parent createContent() {
+        Pane root = new Pane();
+        root.setPrefSize(W, H);
 
-    /* Use it or not, as you wish! */
-    private static boolean DEBUG = false;
+        for (int y = 0; y < Y_TILES; y++) {
+            for (int x = 0; x < X_TILES; x++) {
+                Square tile = new Square(x, y);
 
-    /* if you want to restore output to console */
-    static PrintStream old = System.out;
+                grid[x][y] = tile;
+                root.getChildren().add(tile);
 
-    /* Gets the package name.  The usage assumes that Critter and its
-       subclasses are all in the same package. */
-    private static String myPackage; // package of Critter file.
+            }
+        }
 
-    /* Critter cannot be in default pkg. */
-    static {
-        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+        return root;
     }
 
-    /**
-     * Main method.
-     *
-     * @param args args can be empty.  If not empty, provide two
-     *             parameters -- the first is a file name, and the
-     *             second is test (for test output, where all output
-     *             to be directed to a String), or nothing.
-     */
+    private class Square extends StackPane {
+        private int x, y;
+        private boolean isOpen = false;
+
+        private Rectangle border = new Rectangle(TILE_SIZE - 2, TILE_SIZE - 2);
+        private Text text = new Text();
+
+        public Square(int x, int y) {
+            this.x = x;
+            this.y = y;
+
+            border.setStroke(Color.WHITE);
+            border.setFill(Color.LIGHTGRAY);
+
+            text.setFont(Font.font(18));
+            text.setVisible(false);
+
+            getChildren().addAll(border, text);
+
+            setTranslateX(x * TILE_SIZE);
+            setTranslateY(y * TILE_SIZE);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(createContent());
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-
-        if (args.length != 0) {
-            try {
-                inputFile = args[0];
-                kb = new Scanner(new File(inputFile));
-            } catch (FileNotFoundException e) {
-                System.out.println("USAGE: java <pkg name>.Main OR java <pkg name>.Main <input file> <test output>");
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                System.out.println("USAGE: java <pkg name>.Main OR java <pkg name>.Main <input file> <test output>");
-            }
-            if (args.length >= 2) {
-                /* If the word "test" is the second argument to java */
-                if (args[1].equals("test")) {
-                    /* Create a stream to hold the output */
-                    testOutputString = new ByteArrayOutputStream();
-                    PrintStream ps = new PrintStream(testOutputString);
-                    /* Save the old System.out. */
-                    old = System.out;
-                    /* Tell Java to use the special stream; all
-                     * console output will be redirected here from
-                     * now */
-                    System.setOut(ps);
-                }
-            }
-        } else { // If no arguments to main
-            kb = new Scanner(System.in); // Use keyboard and console
-        }
-        commandInterpreter(kb);
-        System.out.flush();
+        launch(args);
     }
 
-    /* Do not alter the code above for your submission. */
-
-    /**
-     * This function reads in user commands from the keyboard.
-     *
-     * @param kb  This is the keyboard Scanner
-     */
-    private static void commandInterpreter(Scanner kb) {
-        //TODO Implement this method
-        Critter.clearWorld();
-        System.out.print("critters> ");
-        String input = kb.nextLine();
-        System.out.println();
-        String[] commands = input.trim().split("\\s+");
-
-        while (!commands[0].equals("quit")){
-            /////////////////////////////SHOW/////////////////////////////
-            if (commands[0].equals("show")) {
-                if (commands.length == 1) {
-                    Critter.displayWorld();
-                }
-                else {
-                    System.out.println("error processing: " + input);
-                }
-            }
-            /////////////////////////////STEP/////////////////////////////
-            else if (commands[0].equals("step")){
-                if (commands.length > 2){
-                    System.out.println("error processing: " + input);
-                }
-                else{
-                    int count = 1;
-                    if(commands.length > 1){
-                        try {
-                            count = Integer.parseInt(commands[1]);
-                        }
-                        catch (NumberFormatException e) {
-                            System.out.println("error processing: " + input);
-                        }
-                    }
-
-                    for (int i = 0; i < count; i++) {
-                        Critter.worldTimeStep();
-                    }
-                }
-            }
-            /////////////////////////////SEED/////////////////////////////
-            else if (commands[0].equals("seed")){
-                if (commands.length <= 2) {
-                    if (commands.length == 1) { // needs second number for seed
-                        System.out.println("error processing: " + input);
-                    }
-                    else {
-                        try {
-                            Critter.setSeed(Integer.parseInt(commands[1]));
-                        } catch (NumberFormatException e) {
-                            System.out.println("error processing: " + input);
-                        }
-                    }
-                }
-                else { //overall error like "seed  *wrong_string*"
-                    System.out.println("error processing: " + input);
-                }
-            }
-            /////////////////////////////CREATE/////////////////////////////
-            else if (commands[0].equals("create")){
-                if(commands.length > 3) {
-                    System.out.println("error processing: " + input);
-                }
-                int n = 0;
-                if (commands.length == 3) {// custom critter value
-                    try {
-                        n = Integer.parseInt(commands[2]);
-                    } catch (NumberFormatException e) {
-                        System.out.println("error processing: " + input);
-                    }
-                }
-                else { //default critter value
-                    n = 1;
-                }
-                try {
-                    for (int i = 0; i < n; i++) {
-                        Critter.createCritter(commands[1]);
-                    }
-                } catch (InvalidCritterException e) {
-                    System.out.println("error processing: " + input);
-                }
-            }
-            /////////////////////////////STATS/////////////////////////////
-            else if (commands[0].equals("stats")){
-
-                if(commands.length > 2) { // only 1 input for stats
-                    System.out.println("error processing: " + input);
-                }
-                else {
-                    try {
-                        List<Critter> critList = Critter.getInstances(commands[1]);
-                        if (commands[1].equals("Clover") || commands[1].equals("Critter")) {
-                            Critter.runStats(critList);
-                        } else {
-                            Class<?> critClass = Class.forName(myPackage + "." + commands[1]);
-                            Constructor<?> constructor = critClass.getConstructor();
-                            Object new_critter = constructor.newInstance();
-                            Method statMethod = new_critter.getClass().getDeclaredMethod("runStats", List.class);
-                            statMethod.invoke(critClass, critList);
-                        }
-                    } catch (InvalidCritterException | ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
-                        System.out.println("error processing: " + input);
-                        System.out.println(e);
-                    }
-                }
-            }
-            /////////////////////////////CLEAR/////////////////////////////
-            else if (commands[0].equals("clear")){
-                if (commands.length == 1) {
-                    Critter.clearWorld();
-                }
-                else {
-                    System.out.println("error processing: " + input);
-                }
-            }
-            /////////////////////////////Quit Error/////////////////////////////
-            else {
-                System.out.println("invalid command: " + input); // invalid input
-            }
-
-            System.out.print("critters> ");
-            input = kb.nextLine();
-            System.out.println();
-            commands = input.trim().split("\\s+");
-        }
-
-
-    }
 }
